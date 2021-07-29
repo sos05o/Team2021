6a4d002e05cab5ef66a3bc53bdba55ed1e075923
arihara ryunosuke <r.arihara.sys20@morijyobi.ac.jp>
2021-07-29 09:47:13 +0900

    [add] slack_message.pyの作成
    slack_botから、引数に与えたメールアドレス宛に、DMを送る機能を作成

---
+++ Team2021/slack_message.py
@@ -0,0 +1,43 @@
+from slack_bolt import App
+from slack_sdk.errors import SlackApiError
+import os
+
+try:
+    import slack_bot_token_file as token_file
+except KeyError as e:
+    print(e)
+    exit()
+
+app = App(
+    token=token_file.token,
+)
+
+
+def send_message_for_email(mail_message: list):
+    """mail_message[{email:string, message:string}, {}, {}...]"""
+    for array in mail_message:
+        data = get_user_id_for_email(email_addr=array['email'])
+        if not data['ok']:
+            exit()
+        try:
+            app.client.chat_postMessage(channel=data['user']['id'], text=array['message'])
+            return True
+        except SlackApiError as e:
+            print(e)
+            return False
+
+
+def get_user_id_for_email(email_addr):
+    try:
+        id_dict = app.client.users_lookupByEmail(email=email_addr)
+    except SlackApiError:
+        print('The user_id corresponding to the email address used was not found.')
+        return {'ok': False}
+    return id_dict
+
+
+if __name__ == '__main__':
+    message_list = [
+        {"email": "k.sugisawa.sys20@morijyobi.ac.jp", "message": "send as test message"}
+    ]
+    send_message_for_email(message_list)
